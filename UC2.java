0234c443b017c0dcb1a60e1d0882bc8c65897e96
Sakshibansal0211 <sakshibansal0211@gmail.com>
2026-02-16 00:53:13 +0530

    Update UC2.java

--- UC2.java
+++ UC2.java
@@ -1,5 +1,5 @@
 public class UC2{
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         System.out.println("  ***      ***    ******    ******  ");
         System.out.println(" ** **    ** **   **    **  **      ");
