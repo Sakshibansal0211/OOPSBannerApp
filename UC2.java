85540a650dc9b4ad7f68471fcea359cd5a0f7388
Sakshibansal0211 <sakshibansal0211@gmail.com>
2026-02-15 17:50:06 +0530

    Added

--- UC2.java
+++ UC2.java
@@ -1,14 +1,14 @@
-public class UC2{
+public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println("   ***      ***    ******    *****  ");
-        System.out.println("  ** **    ** **   **    **  **   ** ");
-        System.out.println(" **   **  **   **  **    **  **    **");
-        System.out.println(" **   **  **   **  **    **  **   ** ");
-        System.out.println(" **   **  **   **  ******    *****  ");
-        System.out.println(" **   **  **   **  **        **     ");
-        System.out.println("  ** **    ** **   **        **     ");
-        System.out.println("   ***      ***    **        **     ");
+        System.out.println("  ***      ***    ******    ******  ");
+        System.out.println(" ** **    ** **   **    **  **      ");
+        System.out.println("**   **  **   **  **    **  **      ");
+        System.out.println("**   **  **   **  ******     ****** ");
+        System.out.println("**   **  **   **  **              **");
+        System.out.println("**   **  **   **  **              **");
+        System.out.println(" ** **    ** **   **        **    **");
+        System.out.println("  ***      ***    **         ****** ");
 
     }
 }
